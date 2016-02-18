28dc45d5a2a61efa87c3861d3c590b7f6a592b3f
shankar2105 <shankar21mail@gmail.com>
2016-02-18 20:12:02 +0530

    Write temp file upload

--- app/app.js
+++ app/app.js
@@ -2,3 +2,6 @@
 // import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
 // import jetpack from 'fs-jetpack'; // module loaded from npm
+import * as tempFile from './api/controllers/write_temp_file';
+
+window.tempFile = tempFile;
--- app/scripts/controllers/sample_template_controller.js
+++ app/scripts/controllers/sample_template_controller.js
@@ -1,13 +1,24 @@
 /**
  * Sample site controller
  */
-window.maidsafeDemo.controller('SampleTemplateCtrl', [ '$scope', 'safeApiFactory', function($scope, safe) {
+window.maidsafeDemo.controller('SampleTemplateCtrl', [ '$scope', '$http', 'safeApiFactory', function($scope, $http, safe) {
   'use strict';
   $scope.siteTitle = 'My Page';
   $scope.siteDesc = 'This page is created and published on the SAFE Network using the SAFE Uploader';
+  var filePath = './views/sample_template_layout.html';
+
+  var writeFile = function(title, content, filePath) {
+    window.tempFile.writeTempFile(title, content, filePath, function(err, data) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(data);
+    });
+  };
 
   $scope.publish = function() {
     console.log($scope.siteTitle + " " + $scope.siteDesc);
+    writeFile($scope.siteTitle, $scope.siteDesc, filePath);
   };
 
 } ]);
