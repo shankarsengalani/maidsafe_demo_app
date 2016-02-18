3d12abbebc01b4f3549f95459964bfebeccf0282
shankar2105 <shankar21mail@gmail.com>
2016-02-18 08:13:34 +0530

    Public ID integrated

--- app/scripts/factories/dns_factory.js
+++ app/scripts/factories/dns_factory.js
@@ -5,6 +5,17 @@ window.maidsafeDemo.factory('dnsFactory', [ function(Shared) {
   'use strict';
   var self = this;
 
+  self.createPublicId = function(longName, callback) {
+    var payload = {
+      url: this.SERVER + 'dns/' + longName,
+      method: 'POST',
+      headers: {
+        authorization: 'Bearer ' + this.authToken
+      }
+    };
+    (new this.Request(payload, callback)).send();
+  };
+
   // get dns list
   self.getDns = function(callback) {
     var payload = {
